e1cb2260ea74a8b056145836b033e874a19a4920
Surya D <suryadcmech@gmail.com>
2026-05-27 12:12:37 +0530

    Added JavaScript practice exercises

---
+++ script.js
@@ -0,0 +1,30 @@
+let a = 10;
+let b = 10;
+
+let operator = "+"
+
+if (operator === "+"){
+    console.log(a + b);
+    
+}
+
+else if (operator === "-"){
+    console.log(a - b);
+    
+}
+
+else if (operator === "*"){
+    console.log(a * b);
+    
+}
+
+else if (operator === "/"){
+    console.log(a / b);
+    
+}
+
+else {
+    console.log("Invalid operator");
+    
+}
+
