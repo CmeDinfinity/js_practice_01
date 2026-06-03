012f9a402189d2e64785691c11e67e08cca7c05a
Surya D <suryadcmech@gmail.com>
2026-06-03 13:18:12 +0530

    New array

---
+++ array.js
@@ -0,0 +1,58 @@
+// const marks = [90,30,45,85,75];
+
+// const passmarks = [];
+
+// for(let i = 0; i < marks.length; i = i + 1){
+//     if (marks[i] >= 50){
+//         passmarks.push(marks[i])
+//     }
+// }
+
+// console.log(passmarks);
+
+
+// 1. For loop
+// const numbers = [10, 20, 30, 40];
+
+// for(let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i]);
+    
+// }
+
+//2.Loop through an object
+
+// const student = {
+//   name: "Surya",
+//   age: 27,
+//   grade: "A"
+// };
+
+// for (let i in student){
+//     console.log(i,student[i]);
+    
+// }
+
+// 3. using map
+
+// const marks = [50, 60, 70, 80];
+
+// new_marks = marks.map(marks =>marks - 10)
+
+// console.log(new_marks);
+
+
+// 4.Filter
+// const values = [5, 12, 8, 25, 3, 15];
+
+// const filtered_values = values.filter(values => values > 10)
+
+// console.log(filtered_values);
+
+// 5.reduce
+
+const nums = [5, 10, 15, 20];
+
+const reduced_nums = nums.reduce((sum, nums) => sum + nums)
+
+console.log(reduced_nums);
+
